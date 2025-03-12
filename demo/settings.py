bc14ff295c130d8f24d78f41a4265d0c0a2c2a1e
eileen <eileen.gmail.com>
2025-03-13 00:53:44 +0800

    3rd

--- demo/settings.py
+++ demo/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'demo',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '123456'
     }
 }
 
