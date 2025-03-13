373505e742e3e20452a67d3e24f979bb51d87761
eileen <eileen.gmail.com>
2025-03-14 00:33:44 +0800

    5th

--- account/models.py
+++ account/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from utils.basemodels import BaseModel
+
 
 # Create your models here.
-class User(models.Model):
-  id = models.IntegerField('用户id',primary_key=True)
+class User(BaseModel):
+  id = models.AutoField('用户id',primary_key=True)
   username = models.CharField('用户名', max_length=10, null=True, blank=True)
   password = models.CharField('密码', max_length= 18)
   email = models.EmailField('邮箱')
+
+  class Meta:
+    db_table = 'user'
+    verbose_name = '用户'
--- app01/models.py
+++ app01/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from account.models import User
+from utils.basemodels import BaseModel
 # Create your models here.
-class Article(models.Model):
+class Article(BaseModel):
   id = models.IntegerField(primary_key=True)
   title = models.CharField('标题', max_length=100)
   content = models.TextField('内容')
   publish_date = models.DateTimeField('出版日期')
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'article'
+    verbose_name = '文章'
+    ordering = ['-publish_date']
+
