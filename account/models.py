7652444013ee9429e7243fb57a4439115ca119c1
Orieileen <2290912185@qq.com>
2025-03-13 18:21:43 +0800

    4th

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  id = models.IntegerField('用户id',primary_key=True)
+  username = models.CharField('用户名', max_length=10, null=True, blank=True)
+  password = models.CharField('密码', max_length= 18)
+  email = models.EmailField('邮箱')
--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from account.models import User
 # Create your models here.
+class Article(models.Model):
+  id = models.IntegerField(primary_key=True)
+  title = models.CharField('标题', max_length=100)
+  content = models.TextField('内容')
+  publish_date = models.DateTimeField('出版日期')
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
