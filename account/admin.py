caf49adeb68d76d301f260352d6c493dfb3e76b7
Orieileen <2290912185@qq.com>
2025-03-15 11:57:10 +0800

    6th

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from account.models import User
 
-# Register your models here.
+# 导入Django admin模块和User模型
+
+class UserAdmin(admin.ModelAdmin):
+    """
+    用户管理类
+    用于在Django admin后台管理用户信息
+    """
+    # 设置在后台admin列表页面显示的字段，例如：显示username和email，但是models.py中将这两个值赋予了verbose_name，所以实际显示为verbose_name
+    list_display = ('username','email')
+    # 配置过滤字段(例：根据以下字段做的筛选器)
+    list_filter = ('username','email')
+# 注册User模型到admin后台,使用UserAdmin类进行管理
+admin.site.register(User, UserAdmin)
--- account/models.py
+++ account/models.py
@@ -4,11 +4,24 @@ from utils.basemodels import BaseModel
 
 # Create your models here.
 class User(BaseModel):
+  """
+  用户模型类
+  用于存储用户基本信息
+  """
+  # 用户ID,自增主键
   id = models.AutoField('用户id',primary_key=True)
+  # 用户名,最大长度10个字符,允许为空
   username = models.CharField('用户名', max_length=10, null=True, blank=True)
+  # 密码,最大长度18个字符
   password = models.CharField('密码', max_length= 18)
-  email = models.EmailField('邮箱')
+  # 邮箱地址
+  email = models.EmailField('邮箱')  # '邮箱'是这个字段在Django admin后台显示的中文名称,也叫verbose_name
 
   class Meta:
+    """
+    元数据类
+    """
+    # 指定数据库表名
     db_table = 'user'
+    # 在admin后台显示的名称
     verbose_name = '用户'
